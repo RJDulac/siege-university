5d0944d23a9af73a8b7b7df8a37533373cae17e3
RJDulac <analyticrye@gmail.com>
2020-10-05 17:23:34 -0400

    gave function ability to assign inline css and added two needed styles for it

--- js/main.js
+++ js/main.js
@@ -8,9 +8,55 @@ const tutorial = () => {
     "Opening An Example",
     "Basic Movement Controls Within SE",
   ];
+  const nodeWorkSetences = [
+    "Create A Map",
+    "Create A Region",
+    "Add Floors And Walls",
+  ];
+  const objectsSentences = [
+    "Add Details To House",
+    "Add Details To Surrounding Farmland",
+    "Add Starting Positions",
+  ];
   createSingleElement("The Basics of the Siege Editor", "H2");
   createSingleLink("Introduction");
-  createElementsFromArray(introductionSentences, root, "P");
+  createElementsFromArray(introductionSentences, root, "LI");
+  createSingleLink("101: Nodework");
+  createElementsFromArray(nodeWorkSetences, root, "LI");
+  createSingleLink("102: Objects");
+  createElementsFromArray(objectsSentences, root, "LI");
+  createSingleLink("103: Lighting");
+  createElementsFromArray(
+    ["Ambient Light", "Directional Lights", "Point Light"],
+    root,
+    "LI"
+  );
+  createSingleLink("104: Fading");
+  createElementsFromArray(
+    ["Node Camera Flags", "Fading Nodes With Triggers"],
+    root,
+    "LI"
+  );
+  createElementsFromArray(
+    [
+      "105: Moods",
+      "106: Monster Placement",
+      "107: Opening Your Map in Dungeon Siege",
+      "Conclusion",
+      "Appendix A: Tips",
+      "Appendix B: Siege Editor Preferences",
+    ],
+    root,
+    "A"
+  );
+
+  createSingleElement("Introduction", "h2", root, ["italic", "underline"]);
+
+  const introductionParagraphs = [
+    'Building a region for Dungeon Siege using the Siege Editor ("SE") is a straightforward task, even for a beginning mod maker. This Siege U tutorial has two purposes: familiarization with the Siege Editor and familiarization with Siege University. At the conclusion of this tutorial, you will be familar with basic SE functionality and will possess an understanding of how Siege U works.',
+    "This tutorial will walk you through building a simple, small region. It will not delve into all the options of each section, nor will it offer much more than a cursory explanation of the motives of building a region. These topics are covered in further detail in the Siege Editor Manual (included during the installation of the Siege Editor) and further, more specific Siege U tutorials.",
+  ];
+  createElementsFromArray(introductionParagraphs, root, "P");
 };
 
 //helper functions to create elements from arrays
@@ -33,9 +79,33 @@ const createSingleLink = (text, appendFrom = root, reference = "#") => {
   link.innerText = text;
 };
 
-const createSingleElement = (text, elementType, appendFrom = root) => {
+const createSingleElement = (
+  text,
+  elementType,
+  appendFrom = root,
+  styles = "none"
+) => {
   const name = document.createElement(elementType);
   appendFrom.appendChild(name);
   name.innerText = text;
+
+  if (styles !== "none") {
+    assignStyles(name, styles);
+  }
+};
+
+const assignStyles = (name, styles) => {
+  for (styleType of styles) {
+    switch (styleType) {
+      case "italic":
+        name.style.fontStyle = "italic";
+        break;
+      case "underline":
+        name.style.textDecoration = "underline";
+        break;
+      default:
+        console.log("something went wrong");
+    }
+  }
 };
 tutorial();
